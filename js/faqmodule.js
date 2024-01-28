25d2186fa9bb67bc3eace00896201a213c255947
giorgivashakmadze <vashakmadze91@gmail.com>
2024-01-28 21:51:33 +0400

    comment

--- js/faqmodule.js
+++ js/faqmodule.js
@@ -1,7 +1,7 @@
 export function toggleText(element) {
     const hiddenText = element.nextElementSibling;
     const arrowIcon = element.querySelector('.fa-solid');
-
+    //changes arrow dirrection and display style on click
     if (hiddenText.style.display === "none") {
         hiddenText.style.display = "block";
         arrowIcon.classList.remove("fa-angle-down");
